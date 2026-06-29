dbeaa6e6020619687e810e03a3676a505dcd2838
BHAWANI-SINGH-CODE <rajputbhawani2813145@gmail.com>
2026-06-29 13:44:52 +0530

    chore: initial project setup

---
+++ ai-resume-job-matching-platform/src/main/java/com/bhawanisingh/airesume/AiResumeJobMatchingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.bhawanisingh.airesume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiResumeJobMatchingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiResumeJobMatchingPlatformApplication.class, args);
+	}
+
+}
---
+++ ai-resume-job-matching-platform/src/test/java/com/bhawanisingh/airesume/AiResumeJobMatchingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhawanisingh.airesume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiResumeJobMatchingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
