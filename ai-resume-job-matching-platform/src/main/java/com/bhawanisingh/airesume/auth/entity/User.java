7c97ed67cb75f30075aea28aef68bc74cadcbc78
BHAWANI-SINGH-CODE <rajputbhawani2813145@gmail.com>
2026-07-01 10:14:20 +0530

    Sprint 2 Step 4 - Added User Entity

---
+++ ai-resume-job-matching-platform/src/main/java/com/bhawanisingh/airesume/auth/entity/User.java
@@ -0,0 +1,67 @@
+package com.bhawanisingh.airesume.auth.entity;
+
+import com.bhawanisingh.airesume.auth.enums.Role;
+import com.bhawanisingh.airesume.auth.enums.UserStatus;
+import com.bhawanisingh.airesume.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(
+        name = "users",
+        indexes = {
+                @Index(name = "idx_user_email", columnList = "email")
+        },
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_user_email",
+                        columnNames = "email"
+                )
+        }
+)
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "full_name",
+            nullable = false,
+            length = 100)
+    private String fullName;
+
+    @Column(nullable = false,
+            length = 255)
+    private String email;
+
+    @Column(nullable = false,
+            length = 255)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false,
+            length = 20)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false,
+            length = 30)
+    private UserStatus status;
+
+    @Column(name = "email_verified",
+            nullable = false)
+    @Builder.Default
+    private Boolean emailVerified = false;
+
+    @Column(name = "profile_image_url",
+            length = 500)
+    private String profileImageUrl;
+
+}
